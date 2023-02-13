c51f74481962566345936f909055a80b2ef0b670
FLzyy <flzyy72@gmail.com>
2023-02-13 20:08:11 +0800

    don't poll

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,12 +1,4 @@
 import { defineConfig } from "astro/config";
 
 // https://astro.build/config
-export default defineConfig({
-  vite: {
-    server: {
-      watch: {
-        usePolling: true,
-      },
-    },
-  },
-});
+export default defineConfig({});
